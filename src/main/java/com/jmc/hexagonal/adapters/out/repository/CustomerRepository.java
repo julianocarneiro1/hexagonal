6f7adc955fc3848bb6239fa8e448aa0103601b55
Juliano Carneiro <julianoomc@gmail.com>
2024-07-29 17:14:53 -0300

    ajusta CustomerRepository

--- src/main/java/com/jmc/hexagonal/adapters/out/repository/CustomerRepository.java
+++ src/main/java/com/jmc/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.jmc.hexagonal.adapters.out.repository;
 import com.jmc.hexagonal.adapters.out.repository.entity.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class CustomerRepository extends MongoRepository<CustomerEntity, String> {
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
 }
