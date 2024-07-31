6495015fac154836c7cfdd2db096aaea04480eb7
Juliano Carneiro <julianoomc@gmail.com>
2024-07-31 12:09:25 -0300

    cria usecase para deleção de cliente

---
+++ src/main/java/com/jmc/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,22 @@
+package com.jmc.hexagonal.application.core.usecase;
+
+import com.jmc.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.jmc.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort,
+                                     DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+        deleteCustomerByIdOutputPort.delete(id);
+    }
+}
