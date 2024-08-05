d8e75e5dad3e1c642e1c64a636d171e035e3ee5e
Juliano Carneiro <julianoomc@gmail.com>
2024-08-05 14:55:20 -0300

    cria producer para envio do cpf ao topico do kafka

--- src/main/java/com/jmc/hexagonal/config/InsertCustomerConfig.java
+++ src/main/java/com/jmc/hexagonal/config/InsertCustomerConfig.java
@@ -2,6 +2,7 @@ package com.jmc.hexagonal.config;
 
 import com.jmc.hexagonal.adapters.out.FindAddressByZipCodeAdapter;
 import com.jmc.hexagonal.adapters.out.InsertCustomerAdapter;
+import com.jmc.hexagonal.adapters.out.SendCpfValidationAdapter;
 import com.jmc.hexagonal.application.core.usecase.InsertCustomerUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,9 @@ public class InsertCustomerConfig {
     @Bean
     public InsertCustomerUseCase insertCustomerUseCase(
             FindAddressByZipCodeAdapter findAddressByZipCodeAdapter,
-            InsertCustomerAdapter insertCustomerAdapter
+            InsertCustomerAdapter insertCustomerAdapter,
+            SendCpfValidationAdapter sendCpfValidationAdapter
     ) {
-        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter);
+        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter, sendCpfValidationAdapter);
     }
 }
